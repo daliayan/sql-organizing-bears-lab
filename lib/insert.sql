22b83c841e1c37e823e49b8c6d27875b1620547c
daliayan <daliayan5@gmail.com>
2021-03-14 16:29:52 -0400

    I built the table for this sql Bear Organizer, inserted values, and selected statements

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 3, "M", "Brown", "Kind", true);
+INSERT INTO bears VALUES (2, "Rowdy", 31, "M", "dark brown", "Rowdy", false); 
+INSERT INTO bears VALUES (3, "Tabitha", 4, "F", "dark brown", "Shy", true);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 12, "M", "Blue", "Stern", true); 
+INSERT INTO bears VALUES (5, "Melissa", 26, "F", "dark brown", "Sassy", false); 
+INSERT INTO bears VALUES (6, "Grinch", 47, "M", "Green", "Grumpy", true); 
+INSERT INTO bears VALUES (7, "Wendy", 1, "F", "Chocolate", "Sweet", true); 
+INSERT INTO bears VALUES (8, NULL, 301, "M", "Purple", "Mysterious", false); 
